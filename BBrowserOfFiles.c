b03e4fc9fb899098740fc9bbc4d4ad0e5174da76
JAnt5 <jure.antolicc@gmail.com>
2026-07-04 10:15:25 +0200

    cleaner less -object oriented- aproach and first working version

--- BBrowserOfFiles.c
+++ BBrowserOfFiles.c
@@ -1,164 +1,136 @@
 #include <curses.h>
 #include <dirent.h>
 #include <ncurses.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <term.h>
 #include <unistd.h>
 
-int SCREEN_WIDTH = 100;
-const int MENU_HEIGHT = 6;
-int folder_error = 0;
+#define MAX_FILE_NAME 255
 
+void file_menu(WINDOW *menu_win, int highlight, int x, int y, int n_choices,
+               char *choices[]) {
+  int i;
+  x += 2;
+  y += 2;
 
-WINDOW *create_newwin(int height, int width, int starty, int startx) {
-  WINDOW *local_win;
-
-  local_win = newwin(height, width, starty, startx);
-  box(local_win, 0, 0);
-  wrefresh(local_win);
-
-  return local_win;
-}
-void destroy_win(WINDOW *local_win) {
-  wborder(local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-  wrefresh(local_win);
-  delwin(local_win);
-}
-WINDOW *displayTopWindow(int row, int col) {
-  int height = row - MENU_HEIGHT;
-  int width = col - 1;
-  int starty = row - height - MENU_HEIGHT;
-  int startx = col - width;
-  refresh();
-  WINDOW *window;
-  window = create_newwin(height, width, starty, startx);
-
-  wrefresh(window);
-  return window;
-}
-
-
-WINDOW *displayTerminalMenu(int row, int col, char *input) {
-  int height = MENU_HEIGHT;
-  int width = col - 1;
-  int starty = (row - height);
-  int startx = (col - width);
-  refresh();
-  WINDOW *my_win;
-  my_win = create_newwin(height, width, starty, startx);
-  wborder(my_win, '|', '|', '-', '-', '*', '*', '*', '*');
-  mvwprintw(my_win, 1, 2, "Type 'help' to view controls");
-  mvwprintw(my_win, 3, 2, "Command:");
-  mvwchgat(my_win, 3, 2, 8, A_BLINK, 1,
-           NULL); // Command: v tej funkciji rabis napisat koliko characterjev
-                  // od koordinate bo blinkal zato je 8
-
-  attron(A_BOLD);
-  mvwprintw(my_win, 3, 12, "%s", input);
-  attroff(A_BOLD);
-
-  wrefresh(my_win);
-
-  // destroy_win(my_win);
-  return my_win;
-}
-WINDOW *displayHelpWindow(int row, int col, WINDOW *window) {
-
-  int x = 2, y = 2;
-  mvwprintw(window, x, y, "> help -> Brings up this page");
-  mvwprintw(window, x += 2, y, "> exit -> Quits the program");
-  mvwprintw(window, x += 2, y, "> cd 'path' -> Move to that specific path");
-  mvwprintw(window, x += 2, y, ">'arrow up' -> Move the cursor up");
-  mvwprintw(window, x += 2, y, ">'arrow down' -> Move the cursor down");
-
-  wrefresh(window);
-  return window;
-}
-
-WINDOW *displayWelcomeScreen(int row, int col, WINDOW *window) {
-  int x = 2, y = 2;
-  mvwprintw(window, x, y, "> WELCOME TO BBROWSER OF FILES");
-  mvwprintw(window, x += 2, y, "> type 'help' to view commands");
-  mvwprintw(window, x += 2, y,
-            "> type cd 'path' -> To begin and view a specific folder");
-
-  wrefresh(window);
-  return window;
-}
-WINDOW *FileBrowser(int row, int col, char *path, WINDOW *window) {
-    char dir[100];
-    char *files[256] = {};
-    struct dirent *de;
-    path += 3; // odreže vn cd  pa presledek
-
-    if (chdir(path) != 0){
-      //mvwprintw(window,2,2,"Chdir failed" );
-      //wrefresh(window);
-
-      //return window;
-    }  
-
-    mvwprintw(window, 2, 2, "Current working directory: %s",getcwd(dir,100));
-
-    DIR *dr = opendir(dir);
-    int x = 2;
-    int y = 2;
-    if (dr == NULL)
+  box(menu_win, 0, 0);
+  for (i = 0; i < n_choices; ++i) {
+    if (highlight == i + 1) /* High light the present choice */
     {
-        mvwprintw(window,2,2,"Could not open current directory" );
-        wrefresh(window);
-        return 0;
-    }
-
-    while((de = readdir(dr)) != NULL){
-          mvwprintw(window,y+=2, x, "%s", de ->d_name);
-    }
-    
-    wrefresh(window);
-
-    return window;
-
+      wattron(menu_win, A_REVERSE);
+      mvwprintw(menu_win, y, x, "%s", choices[i]);
+      wattroff(menu_win, A_REVERSE);
+    } else
+      mvwprintw(menu_win, y, x, "%s", choices[i]);
+    ++y;
+  }
+  wrefresh(menu_win);
 }
 
 int main() {
-  int run = 1;
-  char ch[100];
-  int row, col;
-  WINDOW *menu, *help_menu, *helper_window, *welcome_screen, *file_browser;
+
   initscr();
-  // raw();
-  cbreak();
-  keypad(stdscr, TRUE);
-  echo();
+  int xMax, yMax;
+  getmaxyx(stdscr, yMax, xMax);
+  WINDOW *window = newwin(yMax, xMax, 0, 0);
+  keypad(window, TRUE);
 
-  // Setup for the program
-  getmaxyx(stdscr, row, col); /* get the number of rows and columns */
-  menu = displayTerminalMenu(row, col, " ");
-  welcome_screen = displayWelcomeScreen(row, col, displayTopWindow(row, col));
-  while (run) {
+  box(window, 0, 0);
 
-    getmaxyx(stdscr, row, col); /* get the number of rows and columns */
-    wmove(menu,3,12);
-    wclrtoeol(menu);
-    wrefresh(menu);
-    wgetstr(menu, ch);
-    if (strcmp(ch, "exit") == 0) {
-      run = 0;
-    } else if (strcmp(ch, "help") == 0) {
-      help_menu = displayHelpWindow(row, col, displayTopWindow(row, col));
-    } else{
-        
-        file_browser =FileBrowser(row, col,ch, displayTopWindow(row, col));
+  refresh();
+  char dir[256] = "";
+  struct dirent *de;
+  DIR *dr;
+
+  int x, y, file_count, highlight = 1, choice = 0, c;
+  while (1) {
+    highlight = 1;
+    choice = 0;
+    wclear(window);
+
+    wrefresh(window);
+    x = 2;
+    y = 2;
+    if (chdir(dir) != 0) {
     }
-    menu = displayTerminalMenu(row, col, ch);
 
-    refresh(); 
-    
+    mvwprintw(window, y, x, "Current working directory: %s", getcwd(dir, 100));
+
+    dr = opendir(dir);
+    if (dr == NULL) {
+      mvwprintw(window, y, x + 50, "Could not open current directory");
+      wrefresh(window);
+      return 0;
+    }
+
+    file_count = 0;
+    while ((de = readdir(dr)) != NULL) {
+      file_count++;
+    }
+    rewinddir(dr);
+
+    // Dynamic memory allocation and array
+    char **file_list = NULL;
+    file_list = (char **)malloc(file_count * sizeof(char *));
+    for (int i = 0; i < file_count; i++) {
+      file_list[i] = (char *)malloc((MAX_FILE_NAME + 1) * sizeof(char));
+      if (file_list[i] == NULL) {
+        mvwprintw(window, y, x + 50, "Could not allocate memory");
+        wrefresh(window);
+
+        return 0;
+      }
+      // Making a lsit
+      if ((de = readdir(dr)) != NULL) {
+        strcpy(file_list[i], de->d_name);
+      }
+    }
+    // za ponovni read
+    file_menu(window, highlight, x, y, file_count, file_list);
+    while (1) {
+      c = wgetch(window);
+
+      switch (c) {
+      case KEY_UP:
+        if (highlight == 1)
+          highlight = file_count;
+        else
+          --highlight;
+        break;
+      case KEY_DOWN:
+        if (highlight == file_count)
+          highlight = 1;
+        else
+          ++highlight;
+        break;
+      case KEY_BACKSPACE:
+        break;
+      case 10:
+        choice = highlight;
+        break;
+      default:
+        break;
+        mvwprintw(window, y, x + 30, "wrong char bro");
+        wrefresh(window);
+      }
+      file_menu(window, highlight, x, y, file_count, file_list);
+      if (choice != 0) {
+        break;
+      }
+    }
+    strcpy(dir,file_list[choice-1]);
+
+    // freeing memory
+    for (int i = 0; i < file_count; i++) {
+      free(file_list[i]);
+    }
+    // Free memory for the array of pointers
+    free(file_list);
+    closedir(dr);
   }
+  //getch();
+  endwin();
 
-  endwin(); 
   return 0;
 }
